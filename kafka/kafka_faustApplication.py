62660ecb4041d8e77b7e42c14e1f3b487e7ac544
Kirthi Shanbhag <Kirthi.Shanbhag@wdc.com>
2020-08-02 13:42:47 -0700

    adding a new sript for creating a new faust app

---
+++ kafka/kafka_faustApplication.py
@@ -0,0 +1,29 @@
+import faust
+
+#
+# Create the faust app with a name and broker
+#       See: https://faust.readthedocs.io/en/latest/userguide/application.html#application-parameters
+#
+app = faust.App("demo-app",broker="localhost:9092")
+
+#
+#  Connect Faust to com.udacity.streams.clickevents
+#       See: https://faust.readthedocs.io/en/latest/userguide/application.html#app-topic-create-a-topic-description
+#
+topic = app.topic('com.udacity.streams.clickevents')
+
+#
+#  Provide an app agent to execute this function on topic event retrieval
+#       See: https://faust.readthedocs.io/en/latest/userguide/application.html#app-agent-define-a-new-stream-processor
+#
+@app.agent(topic)
+async def clickevent(clickevents):
+    # TODO: Define the async for loop that iterates over clickevents
+    #       See: https://faust.readthedocs.io/en/latest/userguide/agents.html#the-stream
+    # TODO: Print each event inside the for loop
+    async for item in clickevents:
+        print(f"{item}")
+
+if __name__ == "__main__":
+    app.main()
+
